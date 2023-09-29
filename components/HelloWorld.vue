f66e1a2c390a30b94ea22716ce639c2c0c4e7610
PetrRNNT <58854342+PetrRNNT@users.noreply.github.com>
2023-09-29 13:04:07 +0200

    fix template

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,12 +1,65 @@
 <script setup></script>
 
 <template>
-  <h3 class="text-8xl">
-    <span class="text-white drop-shadow">Hell</span>
-    <span class="text-blue-400 drop-shadow">o Wor</span>
-    <span class="text-red-400 drop-shadow">ld!!!</span>
-  </h3>
-  <NuxtIcon name="pepe" />
+  <div class="wrapper">
+    <div class="bg">Nuxt 3</div>
+    <div class="fg">Nuxt 3</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+:root {
+  --gold: #ffb338;
+  --light-shadow: #77571d;
+  --dark-shadow: #3e2904;
+}
+body {
+  margin: 0;
+}
+.wrapper {
+  height: 100vh;
+  width: 100vw;
+  background: radial-gradient(#272727, #1b1b1b);
+  display: grid;
+  grid-template-areas: 'overlap';
+  place-content: center;
+  text-transform: uppercase;
+}
+.wrapper > div {
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: #363833;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 900;
+  font-size: clamp(3em, 18vw, 15rem);
+  grid-area: overlap;
+  letter-spacing: 1px;
+  -webkit-text-stroke: 4px transparent;
+}
+div.bg {
+  background-image: repeating-linear-gradient(
+    105deg,
+    var(--gold) 0%,
+    var(--dark-shadow) 5%,
+    var(--gold) 12%
+  );
+  color: transparent;
+  filter: drop-shadow(5px 15px 15px black);
+  transform: scaleY(1.05);
+  transform-origin: top;
+}
+div.fg {
+  background-image: repeating-linear-gradient(
+    5deg,
+    var(--gold) 0%,
+    var(--light-shadow) 23%,
+    var(--gold) 31%
+  );
+  color: #1e2127;
+  transform: scale(1);
+}
+</style>
